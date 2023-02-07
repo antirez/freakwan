47f975f53739540bac128ba743c337af6c1e7bca
antirez <antirez@gmail.com>
2023-02-07 22:19:33 +0100

    Minimal text scroller for better output.

--- freakwan.py
+++ freakwan.py
@@ -2,6 +2,32 @@ import machine
 import ssd1306, sx1276, time, urandom, struct
 from machine import Pin, SoftI2C
 
+# This class implements an IRC-alike view for the ssd1306 display.
+# it is possible to push new lines of text, and only the latest N will
+# be shown, handling also text wrapping if a line is longer than
+# the screen width.
+class Scroller:
+    def __init__(self, display):
+        self.display = display # ssd1306 actual driver
+        self.lines = []
+        self.xres = 128
+        self.yres = 64
+        self.rows = 8
+
+    # Update the screen content.
+    def refresh(self):
+        self.display.fill(0)
+        for i in range(len(self.lines)):
+            self.display.text(self.lines[i], 0, i*8, 1)
+        self.display.show()
+
+    # Add a new line, without refreshing the display.
+    def print(self,msg):
+        self.lines.append(msg)
+        self.lines = self.lines[-self.rows:]
+
+# The message object represents a FreakWAN message, and is also responsible
+# of the decoding and encoding of the messages to be sent to the "wire".
 class Message:
     def __init__(self, sender, text, uid=False, ttl=3):
         self.sender = sender
@@ -17,11 +43,17 @@ class Message:
 
     def decode(self,msg):
         try:
-            self.uid,self.ttl = stuct.unpack("LB",msg)
-            self.sender,self.text = str(msg[5:]).split(":")
+            self.uid,self.ttl = struct.unpack("LB",msg)
+            self.sender,self.text = msg[5:].decode("utf-8").split(":")
             return True
         except Exception as e:
+            print(e)
             return False
+
+    def from_encoded(encoded):
+        m = Message("_sender_","_txt_")
+        m.decode(encoded)
+        return m
 
 class FreakWAN:
     def __init__(self):
@@ -40,6 +72,7 @@ class FreakWAN:
         self.display.poweron()
         self.display.text('Starting...', 0, 0, 1)
         self.display.show()
+        self.scroller = Scroller(self.display)
 
         # Init LoRa chip
         self.lora = sx1276.SX1276(LYLIGO_216_pinconfig,self.receive_callback)
@@ -63,10 +96,9 @@ class FreakWAN:
         return nick
 
     def receive_callback(self,lora_instance,packet,RSSI):
-        self.display.fill(0)
-        self.display.text(packet, 0, 0, 1)
-        self.display.text(str(RSSI), 0, 15, 1)
-        self.display.show()
+        m = Message.from_encoded(packet)
+        self.scroller.print(m.sender+"> "+m.text)
+        self.scroller.refresh()
 
     def run(self):
         counter = 0
@@ -74,6 +106,7 @@ class FreakWAN:
             msg = Message(sender=self.device_hw_nick(),
                          text="Hi "+str(counter))
             self.lora.send(msg.encode())
+            self.scroller.print("you> "+msg.text)
             time.sleep(5) 
             counter += 1
 
