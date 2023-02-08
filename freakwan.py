0ee5c147185616a1683d5246e82fad6ea2b155ab
antirez <antirez@gmail.com>
2023-02-08 10:02:20 +0100

    Final message header format.

--- freakwan.py
+++ freakwan.py
@@ -9,6 +9,14 @@ import machine
 import ssd1306, sx1276, time, urandom, struct
 from machine import Pin, SoftI2C
 
+MessageTypeData = 0
+MessageTypeACK = 1
+MessageTypeHello = 2
+MessageTypeBulkStart = 3
+MessageTypeBulkData = 4
+MessageTypeBulkEND = 5
+MessageTypeBulkReply = 6
+
 # This class implements an IRC-alike view for the ssd1306 display.
 # it is possible to push new lines of text, and only the latest N will
 # be shown, handling also text wrapping if a line is longer than
@@ -36,29 +44,49 @@ class Scroller:
 # The message object represents a FreakWAN message, and is also responsible
 # of the decoding and encoding of the messages to be sent to the "wire".
 class Message:
-    def __init__(self, sender, text, uid=False, ttl=3):
-        self.sender = sender
+    def __init__(self, nick, text, uid=False, ttl=3, mtype=MessageTypeData, sender=False):
+        self.type = mtype
+        self.nick = nick
         self.text = text
         self.uid = uid if uid != False else self.gen_uid()
+        self.sender = sender if sender != False else self.get_this_sender()
         self.ttl = ttl
 
     def gen_uid(self):
         return urandom.getrandbits(32)
 
+    # Get the sender address for this device. We just take 6 bytes
+    # of the device unique ID.
+    def get_this_sender(self):
+        return machine.unique_id()[-6:]
+
+    # Return the sender as a printable hex string.
+    def sender_to_str(self):
+        if self.sender:
+            s = self.sender
+            return "%02x%02x%02x%02x%02x%02x" % s[0],s[1],s[2],s[3],s[4],s[5]
+        else:
+            return "ffffffffffff"
+
     def encode(self):
-        return struct.pack("LB",self.uid,self.ttl)+self.sender+":"+self.text
+        if self.type == MessageTypeData:
+            return struct.pack("<BLB",self.type,self.uid,self.ttl)+self.sender+self.nick+":"+self.text
 
     def decode(self,msg):
         try:
-            self.uid,self.ttl = struct.unpack("LB",msg)
-            self.sender,self.text = msg[5:].decode("utf-8").split(":")
-            return True
+            type = struct.unpack("<B",msg)
+            if self.type == MessageTypeData:
+                self.type,self.uid,self.ttl,self.sender = struct.unpack("<BLB6s",msg)
+                self.nick,self.text = msg[12:].decode("utf-8").split(":")
+                return True
+            else:
+                return False
         except Exception as e:
-            print(e)
+            print("msg decode error "+str(e))
             return False
 
     def from_encoded(encoded):
-        m = Message("_sender_","_txt_")
+        m = Message("_nick_","_txt_")
         m.decode(encoded)
         return m
 
@@ -104,16 +132,21 @@ class FreakWAN:
 
     def receive_callback(self,lora_instance,packet,RSSI):
         m = Message.from_encoded(packet)
-        self.scroller.print(m.sender+"> "+m.text)
-        self.scroller.refresh()
+        if m:
+            if m.type == MessageTypeData:
+                self.scroller.print(m.nick+"> "+m.text)
+                self.scroller.refresh()
+            else:
+                print("Unknown message type received: "+str(m.type))
 
     def run(self):
         counter = 0
         while True:
-            msg = Message(sender=self.device_hw_nick(),
+            msg = Message(nick=self.device_hw_nick(),
                          text="Hi "+str(counter))
             self.lora.send(msg.encode())
             self.scroller.print("you> "+msg.text)
+            self.scroller.refresh()
             time.sleep(5) 
             counter += 1
 
