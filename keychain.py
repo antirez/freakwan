bb22cbc6501b85bab493f17eb5982f6cfa9aeee6
antirez <antirez@gmail.com>
2023-02-25 17:54:00 +0100

    Keychain fixes.

--- keychain.py
+++ keychain.py
@@ -4,7 +4,8 @@
 # This code is released under the BSD 2 clause license.
 # See the LICENSE file for more information
 
-import cryptolib, hashlib, os
+import cryptolib, hashlib, os, urandom
+from message import *
 
 # This class implements the packets encryption keychain. It loads and
 # saves keys from/to disk, and implements encryption and decryption.
@@ -44,6 +45,10 @@ class Keychain:
         f.close()
         self.load_keys()
 
+    # Return True if the key exists.
+    def has_key(self,key_name):
+        return self.keys.get(key_name) != None
+
     # Return the SHA256 digest truncated to 16 bytes
     def sha16(self,data):
         return hashlib.sha256(data).digest()[:16]
@@ -56,7 +61,9 @@ class Keychain:
             raise Exception("No key with the specified name")
         # Compute the IV and digest with TTL set to 0, as it could
         # change as the packet gets relayed by the network.
-        copy = packet[:3] + b'\x00' + packet[4:]
+        # In the next step we also add the IV field.
+        iv = bytes([urandom.getrandbits(8) for x in range(4)])
+        copy = bytes([packet[0]]) + bytes([packet[1]&(0xff^MessageFlagsRelayed)]) + packet[2:6] + b'\x00' + iv + packet[7:]
         iv = self.sha16(copy[:11])
         checksum = self.sha16(copy)[:9]
         # Set the last byte of the checksum to 1, so that the padding
@@ -64,10 +71,10 @@ class Keychain:
         checksum = checksum[:8] + bytes([checksum[8]|1]) + checksum[9:]
         payload = copy[11:] + checksum
         if (len(payload) % 16): payload += b'\x00' * (16-(len(payload) % 16))
-        encr = cryptolib.aes(key,2,iv).encrypt(payload)
+        encr = cryptolib.aes(key,2,iv).encrypt(payload) # 2 = CBC mode.
         # The final encrypted packet is the original header
-        # plus the encrypted part, from the sender ID to the final checksum.
-        return packet[:11] + encr
+        # plus the IV field and the encrypted part.
+        return packet[:7] + copy[7:11] + encr
 
     # Try every possible key, trying to decrypt the packet. Is
     # no match is found, None is returned, otherwise the method returns
@@ -78,8 +85,8 @@ class Keychain:
             header = encr[:11]
             payload = encr[11:]
 
-            # Set TTL to zero for the IV
-            copy = header[:3] + b'\x00' + header[4:]
+            # Clear Relayed flag and set TTL to zero for the IV
+            copy = bytes([header[0]]) + bytes([header[1]&(0xff^MessageFlagsRelayed)]) + header[2:6] + b'\x00' + header[7:]
             iv = self.sha16(copy)
 
             # Decrypt and see if checksum matches
@@ -93,18 +100,18 @@ class Keychain:
             checksum = self.sha16(copy+plain)[:9]
             checksum = checksum[:8] + bytes([checksum[8]|1]) + checksum[9:]
             if checksum == claimed_checksum:
-                return [key_name,header+plain]
+                return [key_name,header[:7]+plain] # Discard IV field
         return None
 
 if __name__ == "__main__":
     kc = Keychain()
     kc.add_key("freaknet","morte")
-    test_packet = b"TF" + "IDID" + "T" + "IVIV" + "SENDER" + "foo: bar"
+    test_packet = b"TF" + "IDID" + "T" + "SENDER" + "foo: bar"
     #print(test_packet)
     encr = kc.encrypt(test_packet,"freaknet")
-    #print(encr)
+    print("ENCR: "+str(encr))
     decr = kc.decrypt(encr)
-    #print(decr)
+    print("DECR: "+str(decr))
     if test_packet == decr[1]:
         print("Packet encrypted and decrypred with success: got same bytes")
     # Flipping a bit somewhere should no longer result in a valid packet
