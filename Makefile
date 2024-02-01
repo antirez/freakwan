413dd570745630123e4e8995d06b9519384668e7
antirez <antirez@gmail.com>
2024-02-01 15:37:31 +0100

    T-BEAM T22 v1.1 config added.

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
-install: axp192.mpy bt.mpy clictrl.mpy dutycycle.mpy fci.mpy font4x6.mpy freakwan.mpy history.mpy icons.mpy keychain.mpy message.mpy networking.mpy scroller.mpy splash.mpy sx1276.mpy sensor.mpy
-	talk32 /dev/tty.wchusbserial* put *.mpy main.py
-
-%.mpy: %.py
-	mpy-cross $<
+install: device_config.py
+	mpremote cp *.py :
---
+++ devices/device_config.tbeam_t22_v1_1.py
@@ -0,0 +1,35 @@
+### LILYGO T-BEAM T22 v1.1 hardware configuration
+
+from machine import Pin
+from axp192 import AXP192
+
+class DeviceConfig:
+    config = {}
+
+    config['ssd1306']= {
+        'sda': 21,
+        'scl': 22,
+        'xres': 128,
+        'yres': 64,
+    }
+
+    def power_up():
+        DeviceConfig.axp_i2c = SoftI2C(sda=Pin(21), scl=Pin(22))
+        DeviceConfig.axp192 = AXP192(axp_i2c)
+
+    def get_battery_microvolts():
+        return DeviceConfig.axp192.get_battery_volts()*1000000
+
+    config['tx_led'] = {
+        'pin': 4,
+        'inverted': True,
+    }
+
+    config['sx1276'] = {
+        'miso': 19,
+        'mosi': 27,
+        'clock': 5,
+        'chipselect': 18,
+        'reset': 23,
+        'dio0': 26
+    }
